47594e36a2dc21e7be1aca74fe291791c69405fb
Kashem Ali <kasheml45@gmail.com>
2020-03-20 01:57:50 +0600

    Add files via upload

---
+++ inc/userlist.php
@@ -0,0 +1,45 @@
+<?php
+/*
+* @Package: UserList
+*/
+namespace Inc;
+class Userlist {
+     var $plugins_path;
+     function __construct(){
+          $this->plugins_path=plugins_url("",dirname(__FILE__));
+     }
+     function Init(){
+         $methods=array(
+          'userlist_register_menu',
+          'Enquee'
+                        );
+         foreach($methods as $method){
+          
+          add_action( 'admin_menu', array($this,$method) );
+         }
+     }
+     function userlist_register_menu() {
+          add_plugins_page( 'User List', 'User List', 'read', 'user-list',array($this,"userlist_page"));
+     }
+     function userlist_page() {
+    	include_once( 'page.php'  );
+    }
+     function Activate(){
+          flush_rewrite_rules();
+       }
+     function Dectivate(){
+          flush_rewrite_rules();
+     }
+     function Uninstall() {
+          return false;
+      }
+     function Enquee(){
+          wp_enqueue_style("",$this->plugins_path."/assets/style.css");
+          wp_enqueue_style("wp_enqueue_style","//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css");
+           
+           wp_enqueue_script("wp_enqueue_script","//code.jquery.com/jquery-3.4.1.min.js");
+           wp_enqueue_script("jquery-ui","//code.jquery.com/ui/1.12.1/jquery-ui.min.js");
+           wp_enqueue_script("user-list-common",$this->plugins_path."/assets/common.js");
+     }
+
+}
---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+/*
+* Name: UserList
+*/
+function geturl($f_url){
+  return $f_url;
+}
+?>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Users List</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, minimum-scale=1.0, maximum-scale=5.0" />
+   <base href="userfetch"
+	<link rel="stylesheet" href="<?php echo geturl('style.css');?>">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+  <script src="<?php echo geturl('common.js');?>"></script>
+
+<div >
+	<table class="usertable" id="userlist" style="width: 100%;">
+		<caption><h1>Users List</h1> <a onclick="filteruser();">Refresh</a></caption>
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Name</th>
+				<th>Username</th>
+				<th>Email</th>
+				<th>Phone</th>
+				<th>Website</th>
+			</tr>
+		</thead>
+		<tbody>
+
+		</tbody>
+	</table>
+</div>
+	<script>
+    var endpoint = "<?php echo geturl("endpoint/");?>";
+		function filteruser() {
+			$.ajax({
+				url: endpoint,
+				dataType: 'json',
+				success: function(json) {
+					$.map(json, function(item) {
+						item['class'] = "user" + item.id;
+						var html = sprint(`<tr class="{class}">
+						<td align="center" label="ID" ><a onclick="view({id});">{id}</a></td>
+						<td align="center" label="Name"><a onclick="view({id});">{name}</a></td>
+						<td align="center" label="Username"><a onclick="view({id});">{username}</a></td>
+						<td align="center" label="Email">{email}</td>
+						<td align="center" label="Phone">{phone}</td>
+						<td align="center" label="website">{website}</td>
+						
+					</tr>`, item);
+            
+						$('#userlist .' + item['class']).remove();
+						$('#userlist tbody').append(html);
+					});
+				}
+			});
+		}filteruser();
+     var last;
+    function view(id) {
+              $.ajax({
+            dataType: "json",
+            url: endpoint+"?id="+id,
+            success: function(json){     
+               html=sprint(`<table class="table table-bordered table-striped">
+               <tr><td>ID:</td><td>{id}</td></tr>
+               <tr><td>Name:</td><td>{name}</td></tr>
+               <tr><td>UserName:</td><td>{username}</td></tr>
+               <tr><td>Email:</td><td>{email}</td></tr>
+               <tr><td>Phone:</td><td>{phone}</td></tr>
+               <tr><td>Website:</td><td>{website}</td></tr>
+               </table>`,json);
+               if (last) {
+                   $(last).dialog('destroy');
+               }
+                last=$('<div/>').dialog({title:"User Information",show: "slideDown",
+		hide: "slideUp"}).prepend(html);
+            }
+        });
+    }
+     
+	</script>
+</body>
+
+</html>
